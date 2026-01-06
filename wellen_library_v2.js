17cf23b64db044c60fb9f45e275dbb32cc70c879
sveabeiser-pixel <svea.beiser@googlemail.com>
2026-01-06 18:13:39 +0100

    wellenfunktion neu

--- wellen_library_v2.js
+++ wellen_library_v2.js
@@ -248,8 +248,7 @@
 ${fi.input.value || ""}
 `);
       });
-      const blob = new Blob([lines.join("
-")], {type:"text/plain;charset=utf-8"});
+      const blob = new Blob([lines.join("\n")], {type:"text/plain;charset=utf-8"});
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
